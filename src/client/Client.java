c6197bd3575b29fe6904f25cffa96b980597fffb
satrioadityo <satrioadityo@gmail.com>
2015-09-02 02:34:43 +0700

    ngrapihin codingan

--- src/client/Client.java
+++ src/client/Client.java
@@ -7,7 +7,6 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -15,27 +14,12 @@ import javax.swing.JOptionPane;
  */
 public class Client extends Thread{
     
-    private BufferedReader inputStream;
+    static BufferedReader inputStream;
 
     public Client(BufferedReader inputStream) {
         this.inputStream = inputStream;
-        System.err.println("Client just created !");
     }
 
-    @Override
-    public void run() {
-        System.err.println("Thread client just start !");
-        try {
-            String inputan;
-            while((inputan = inputStream.readLine()) != null){
-                System.out.println(inputan);
-                System.out.println(">> ");
-            }
-        } catch (Exception e) {
-        }
-        System.err.println("Thread client just end !");
-    }
-    
     
     /**
      * @param args the command line arguments
@@ -50,32 +34,39 @@ public class Client extends Thread{
             String ip = keyBoard.readLine();
             Socket connect = new Socket(ip, portNumber);
             
-            BufferedReader inputStream = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+            BufferedReader inputStream = new BufferedReader(
+                    new InputStreamReader(connect.getInputStream()));
             
             PrintWriter outputStream = new PrintWriter(connect.getOutputStream(), true);
             
             System.out.println("Just connected to " + connect.getRemoteSocketAddress());
-            
+
             Client in = new Client(inputStream);
             in.start();
             
             String userInput;
                         
             do{
-                System.out.println(">> ");
+                System.out.print(">> ");
                 userInput = keyBoard.readLine();
                 outputStream.println(userInput);
                 outputStream.flush();
             } while(!userInput.equals("quit"));
-            
-//            while((userInput = keyBoard.readLine()) != null){
-//                outStream.println(userInput);
-//                String message = inStream.readLine();
-//                System.out.println("Reply from server : " + message);
-//            }
-            
+                        
         } catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @Override
+    public void run() {
+        try {
+            String inputan;
+            while((inputan = inputStream.readLine()) != null){
+                System.err.println(inputan);
+                System.out.print(">> ");
+            }
+        } catch (Exception e) {
         }
     }
 
